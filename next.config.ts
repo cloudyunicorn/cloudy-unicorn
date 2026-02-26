9d74d8073e735bd39d17757dbb67cc45549d53c8
Rajat Singh <s.rajat55@gmail.com>
2026-02-26 16:54:07 +0530

    updated packages

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,8 @@
 // next.config.ts
-import withPWA from 'next-pwa';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // any other Next.js options you need
 };
 
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-  buildExcludes: [
-    /middleware-manifest\.json$/,
-    /_middleware\.js$/,
-    /_buildManifest\.js$/,
-    /_ssgManifest\.js$/,
-    /app-build-manifest\.json$/,
-  ],
-})(nextConfig);
+export default nextConfig;
